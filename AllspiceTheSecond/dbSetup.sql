da5309f6d93ec54ca44259b0114355ec4804cdb4
Jacob <Jacobzeme8@gmail.com>
2023-03-22 15:28:10 -0600

    init setup

---
+++ AllspiceTheSecond/dbSetup.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS accounts(
+  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  name varchar(255) COMMENT 'User Name',
+  email varchar(255) COMMENT 'User Email',
+  picture varchar(255) COMMENT 'User Picture'
+) default charset utf8 COMMENT '';
+
+CREATE TABLE IF NOT EXISTS recipe(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  instructions VARCHAR(500) NOT NULL,
+  img VARCHAR(500) NOT NULL,
+  catagory VARCHAR(100) NOT NULL,
+  creatorId VARCHAR(255) NOT NULL
+) default charset utf8 COMMENT '';
